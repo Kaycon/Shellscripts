ee22dfdc4c92e4be74b7341fab5714744e86a4da
Kaycon <34174901+Kaycon@users.noreply.github.com>
2021-06-15 14:24:35 +0200

    Added docker compose

--- install_docker.sh
+++ install_docker.sh
@@ -14,5 +14,9 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(
 apt-get update -y
 apt-get install docker-ce -y
 
+#Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 #Check if Docker works
 docker run hello-world
